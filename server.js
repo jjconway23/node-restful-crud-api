d012b03e1164d22fc3d6c73ae7cb7e35f944f9e5
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-20 14:49:45 +0000

    create git ignore file

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res)=> {
+    res.send('Hello Node API')
+})
+
+const PORT = 3000
+app.listen(PORT, ()=> {
+    console.log(`Listening on port ${PORT}`)
+})
