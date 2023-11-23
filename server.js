c89a9a842cf863f95a865d817e44b4f95be618da
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-23 16:12:49 +0000

    complete get product by id functionality

--- server.js
+++ server.js
@@ -13,18 +13,27 @@ app.get('/', (req, res)=> {
 app.get('/blog', (req,res)=> {
     res.send("My Blog")
 })
+app.route('/products')
+    .get(async (req, res)=> {
+        try {
+            const products = await Product.find({})
+            res.status(200).json(products)
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({message:e.message})
+        }
+    })
+    post( async (req, res)=> {
+        try{
+            const product = await Product.create(req.body)
+            res.status(200).send(product)
+        }
+        catch (e) {
+            console.log(e.message)
+            res.status(500).json({message:e.message})
+        }
 
-app.post('/products', async (req, res)=> {
-    try{
-        const product = await Product.create(req.body)
-        res.status(200).send(product)
-    }
-    catch (e) {
-        console.log(e.message)
-        res.status(500).json({message:e.message})
-    }
-
-})
+    })
 mongoose
     .connect('mongodb+srv://root:admin@node-crud-rest-api.fqs1jel.mongodb.net/node-rest-crud-api?retryWrites=true&w=majority')
     .then(()=> {
