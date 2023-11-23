5adc28d25fcdd237c7833bc99f60e61d648fb701
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-23 15:25:57 +0000

    connect app to mongodb database

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send('Hello Node API')
+    res.send('Hello Node API (2)')
 })
 
-const PORT = 3000
-app.listen(PORT, ()=> {
-    console.log(`Listening on port ${PORT}`)
+app.get('/blog', (req,res)=> {
+    res.send("My Blog")
 })
+const PORT = 3000
+
+mongoose
+    .connect('mongodb+srv://root:admin@node-crud-rest-api.fqs1jel.mongodb.net/node-rest-crud-api?retryWrites=true&w=majority')
+    .then(()=> {
+        console.log("Connected to MongoDB")
+        app.listen(PORT, ()=> {
+            console.log(`Listening on port ${PORT}`)
+        })
+    })
+    .catch( (e)=> {
+        console.log(e)
+    })
