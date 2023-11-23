862e0ec8073392b2d2cf3a977aaec8d285d76821
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-23 15:40:42 +0000

    set up app middleware - test post route with dummy data

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const PORT = 3000
+
+app.use(express.json())
 app.get('/', (req, res)=> {
     res.send('Hello Node API (2)')
 })
@@ -9,8 +12,17 @@ app.get('/', (req, res)=> {
 app.get('/blog', (req,res)=> {
     res.send("My Blog")
 })
-const PORT = 3000
 
+app.post('/products', (req, res)=> {
+    try{
+        console.log(req.body);
+        res.send(req.body)
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+})
 mongoose
     .connect('mongodb+srv://root:admin@node-crud-rest-api.fqs1jel.mongodb.net/node-rest-crud-api?retryWrites=true&w=majority')
     .then(()=> {
