4d1d80041a9e189be1beefbcb74ed65b6d22b4bc
JJCONWAY23 <jacob.peat01@gmail.com>
2023-11-23 16:25:26 +0000

    complete functionality to update information

--- server.js
+++ server.js
@@ -6,13 +6,35 @@ const app = express();
 const PORT = 3000
 
 app.use(express.json())
-app.get('/', (req, res)=> {
-    res.send('Hello Node API (2)')
-})
+app.use(express.urlencoded({extened:false}))
 
-app.get('/blog', (req,res)=> {
-    res.send("My Blog")
-})
+app.route('/products/:id')
+    .get(async(req,res)=> {
+        try {
+            const {id} = req.params
+            const product = await Product.findById(id)
+            res.status(200).json(product)
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({message:error.message})
+        }
+
+    })
+    .put(async (req,res)=> {
+        try {
+            const {id} = req.params
+            const product = await Product.findByIdAndUpdate(id, req.body)
+            if(!product){
+                return res.status(404).json({message: `cannot find any product with ID ${id}`})
+            }
+            const updatedProduct = await Product.findById(id)
+            res.status(200).json(updatedProduct)
+
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({message:error.message})
+        }
+    })
 app.route('/products')
     .get(async (req, res)=> {
         try {
@@ -23,7 +45,7 @@ app.route('/products')
             res.status(500).json({message:e.message})
         }
     })
-    post( async (req, res)=> {
+    .post( async (req, res)=> {
         try{
             const product = await Product.create(req.body)
             res.status(200).send(product)
