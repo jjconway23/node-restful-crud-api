ab507faf1648baae10e5d22ada9d9e8b82448ad5
JJCONWAY23 <jacob.peat01@gmail.com>
2024-01-10 15:45:37 +0000

    allow error middleware to throw error

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,74 +1,77 @@
 const Product = require('../models/productModel')
+const asyncHandler = require('express-async-handler')
 
 // Get all Products
-const getProducts = async (req, res)=> {
+const getProducts = asyncHandler(async (req, res)=> {
     try {
         const products = await Product.find({})
         res.status(200).json(products)
     } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message:e.message})
+        res.status(500)
+        throw new Error(error.message)
     }
-}
+})
 
 // Get single Product
-const getProduct = async(req,res)=> {
+const getProduct = asyncHandler(async(req,res)=> {
     try {
         const {id} = req.params
         const product = await Product.findById(id)
         res.status(200).json(product)
     } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message:error.message})
+        res.status(500)
+        throw new Error(error.message)
     }
 
-}
+})
 
 // Create Product
-const createProduct = async (req, res)=> {
+const createProduct = asyncHandler(async (req, res)=> {
     try{
         const product = await Product.create(req.body)
         res.status(200).send(product)
     }
     catch (e) {
-        console.log(e.message)
-        res.status(500).json({message:e.message})
+        res.status(500)
+        throw new Error(error.message)
     }
 
-}
+})
 
 // Update Product
-const updateProduct = async (req,res)=> {
+const updateProduct = asyncHandler(async (req,res)=> {
     try {
         const {id} = req.params
         const product = await Product.findByIdAndUpdate(id, req.body)
         if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+            res.status(404)
+            throw new Error(`Cannot find any product with ID ${id}`)
         }
         const updatedProduct = await Product.findById(id)
         res.status(200).json(updatedProduct)
 
     } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message:error.message})
+        res.status(500)
+        throw new Error(error.message)
     }
-}
+})
 
 // Delete Product
-const deleteProduct = async (req, res)=> {
+const deleteProduct = asyncHandler(async (req, res)=> {
     try {
         const {id} = req.params;
         const product = await Product.findByIdAndDelete(id);
         if(!product){
-            return res.status(404).json({message:`Cannot find any product with ID ${id}`})
+            res.status(404)
+            throw new Error(`Cannot find any product with ID ${id}`)
         }
         res.status(200).json(product)
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message:error.message});
+        res.status(500)
+        throw new Error(error.message)
     }
 
-}
+})
 
 
 
--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoute')
 const errorMiddleware = require('./middleware/errorMiddleware')
 const app = express();
+const cors = require('cors')
 
 const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extened:false}))
 app.use('/api/products', productRoute)
 app.get('/', (req, res)=> {
-    throw new Error('fake error')
+    res.send('Hello NODE API')
 })
 app.use(errorMiddleware)
 
